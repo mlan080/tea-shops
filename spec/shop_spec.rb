e58b910d499ce01f336d6268d398261e21ed55de
Mandy <mandy.lan@gmail.com>
2018-11-20 14:46:41 +0100

    rid whitespaces

--- spec/shop_spec.rb
+++ spec/shop_spec.rb
@@ -2,11 +2,11 @@ require 'rspec'
 require_relative '../app/models/shop'
 
 describe Shop do
-  it 'responds to name' do
+  it 'responds to name' do #description of your spec/test
     shop = Shop.new
 
     expect(shop).to respond_to(:name)
-    #expects shop instance to have an age accessor which it doesn't have
+    #expects shop instance to have an name accessor/method
   end
 
   it 'sums two numbers' do
@@ -15,4 +15,3 @@ describe Shop do
     expect(result).to eq(7)
   end
 end
-
