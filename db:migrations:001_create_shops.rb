dfbee22578d9cc818b637578cf7dee37f790321d
Mandy <mandy.lan@gmail.com>
2018-11-21 11:29:47 +0100

    change from how to use sequel into how to migrate database

--- db:migrations:001_create_shops.rb
+++ db:migrations:001_create_shops.rb
@@ -1,17 +1,13 @@
-require "sequel"
+Sequel.migration do
+  up do #adds a shop table
+    create_table(:shops) do
+      primary_key :id
+      String :name, null: false
+    end
+  end
 
-DB = Sequel.sqlite # connects to sqlite3 using sequel
-
-# create an items table
-DB.create_table :shops do
-  primary_key :id
-  String :name
+  down do
+    drop_table(:shops)
+  end
 end
 
-shops = DB[:shops] #Create a dataset
-
-#populate the table
-shops.insert(:name => 'greentea')
-shops.insert(:name => 'red-cafe')
-
-puts "Shops count: #{shops.count}"
