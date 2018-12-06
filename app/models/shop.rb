77b9b33d17bf449d0cb8bb1de442127efb4a45f3
Mandy <mandy.lan@gmail.com>
2018-12-06 14:49:59 +0100

    Add update instance method and spec (#22)
    * Added find class method and spec for id 15
    
    * Add sequel model and find class method and spec
    
    * Add spec to check if name matches the shops table id = 15
    
    * consistent data type hash
    
    * Add update class method and spec
    
    * Spell out the steps and made id more flexible
    
    * Add sequel model and find class method and spec
    
    * Add spec to check if name matches the shops table id = 15
    
    * consistent data type hash
    
    * Add update class method and spec
    
    * Deleting comments for update class method
    
    *Small polish for update class method
    
    * rspec syntax fixes
    
    * Fixed updates
    
    * fixes2
    
    * testing circle ci works
    
    * last fixes for update
    
    * Delete development.rb
    
    * Delete production.rb
    
    * Delete test.rb

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -4,22 +4,25 @@ DB = Sequel.connect('sqlite://tea-shops.sqlite')
 
 class Shop
   attr_accessor :name, :description #instance methods
+  attr_reader :id
+
   def create
-    shops = DB[:shops] # Define a dataset, table: shops
+    shops = DB[:shops]
     shops.insert(:name => name, :description => description) # Populate the table, :name is column name, name is method accessing the instance variable
   end
 
   def initialize(hash = {})
+    @id = hash[:id]
     @name = hash[:name]
     @description = hash[:description]
   end
 
-  def self.count #class method - used when functionality does not belong to an.instance of that class
+  def self.count
     DB[:shops].count
   end
 
   def self.last
-    DB[:shops].order(:id).last #prevents sequel error: no order specified
+    DB[:shops].order(:id).last
   end
 
   def self.all
@@ -27,7 +30,13 @@ class Shop
   end
 
   def self.find(id)
-    record = DB[:shops].first(id: id) #sequel doc uses first for retrieving record
+    record = DB[:shops].first(:id => id) #sequel doc uses first for retrieving record
+    Shop.new(record) #returns a shop object with hash argument
+  end
+
+  def update
+    DB[:shops].where(id: id).update(name: name)
+    record = DB[:shops].first(id: id)
     Shop.new(record) #returns a shop object with hash argument
   end
 end
--- spec/shop_spec.rb
+++ spec/shop_spec.rb
@@ -3,20 +3,19 @@ require_relative '../app/models/shop'
 
 describe Shop do
   it 'responds to name' do #description of your spec/test
-    shop = Shop.new "cafe" #add name to new to prevent Sequel::NotNullConstraintViolation:
+    shop = Shop.new({name:'cafe', desription: 'pink apples'}) #add name to new to prevent Sequel::NotNullConstraintViolation:
     expect(shop).to respond_to(:name)
     #expects shop instance to have an name accessor/method
   end
 
   describe "#create" do
     it 'increments data row by 1' do
-      shop = Shop.new "cafe"
+      shop = Shop.new({name:'cafe', desription: 'pink apples'})
       expect{shop.create}.to change{Shop.count} #counts, creates then counts again
     end
 
     it 'should have the same attributes' do
-      shop = Shop.new "cafe"
-      shop.description = "hot tea"
+      shop = Shop.new({name:'cafe', desription: 'pink apples'})
       shop.create
       expect(shop.name).to eq(Shop.last[:name]) #[] access column name in db
       expect(shop.description).to eq(Shop.last[:description])
@@ -28,25 +27,37 @@ describe Shop do
     let(:rows) { DB[:shops].all }
 
     it 'returns all rows from the shops table' do
+
       expect(Shop.all).to eq(rows)
     end
   end
 
-  describe "#find" do
-    before { Shop.new({name: 'cafe', description: 'red apples'}).create }
-
-    let(:last_shop) { Shop.all.last } #defining variable to use in spec
+  describe ".find" do
+    let(:shop_id) { Shop.new({name: 'Mandy', description: 'be careful'}).create }
 
     it 'returns the given shop row from shops table' do
-      result = Shop.find(last_shop[:id])
+      result = Shop.find(shop_id)
 
-      expect(result.name).to eq('cafe')
+      expect(result.name).to eq('Mandy')
     end
 
-    it 'returns the instance class' do
-      result = Shop.find(last_shop[:id])
+    it 'returns a Shop instance' do
+      result = Shop.find(shop_id)
 
       expect(result.class).to eq(Shop)
+    end
+  end
+
+  describe "#update" do
+    let(:shop_id) { Shop.new({name: 'Mandy', description: 'be careful'}).create }
+
+    it'updates the shop with name Pandy' do
+      shop = Shop.find(shop_id)
+      shop.name = 'Pandy'
+      shop.update
+      result = Shop.find(shop_id) #reloads from the db
+
+      expect(result.name).to eq('Pandy')
     end
   end
 end
