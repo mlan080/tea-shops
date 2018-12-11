a991b11009f3290cef618808827f5d186bc60445
Mandy <mandy.lan@gmail.com>
2018-12-11 12:35:55 +0100

    database cleaner working

--- spec/database_cleaner.rb
+++
@@ -1,6 +0,0 @@
-require 'database_cleaner'
-require 'sequel'
-
-DatabaseCleaner[:sequel, {:connection => Sequel.connect('sqlite://tea-shops_test.sqlite')}]
-DatabaseCleaner[:sequel].strategy = :truncation
-#DatabaseCleaner.clean
