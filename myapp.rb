bafd8d29ccd85839a8f599354ada4f68bf4eba75
Mandy <mandy.lan@gmail.com>
2019-01-25 15:48:25 +0100

    create endpoint for retrieving a shop

--- myapp.rb
+++ myapp.rb
@@ -2,7 +2,16 @@ require 'sinatra'
 require 'json'
 require_relative 'app/models/shop'
 
+before do
+ content_type :json
+end
+
 get '/v1/shops' do
-  content_type :json
   Shop.all.to_json
 end
+
+get '/v1/shops/:id' do
+  shop = Shop.find(params[:id])
+  shop.to_json
+end
+
