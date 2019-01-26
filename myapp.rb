ba762e8c5df6c01babb942ce7c68f64b33133686
Mandy <mandy.lan@gmail.com>
2019-01-26 17:10:06 +0100

    put request to udpate chosen shop

--- myapp.rb
+++ myapp.rb
@@ -6,3 +6,10 @@ get '/v1/shops' do
   content_type :json
   Shop.all.to_json
 end
+
+put '/v1/shops/:id' do
+  shop = Shop.find(params[:id])
+  shop.name = ""
+  shop.update
+  "204 content not found"
+end
